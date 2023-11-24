a66c0925008dfbcd5af377ead28f83cbf9a25e58
jianshuzhangdc <jianshu.zhang@dcmail.ca>
2023-11-24 12:34:46 -0500

    Finish main.js

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -12,4 +12,33 @@ let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 let insertY = ["the soup kitchen", "Disneyland", "the White House"];
 let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
+randomize.addEventListener('click', result);
 
+function result() {
+	let newStory = storyText;
+	
+	let xItem = randomValueFromArray(insertX);
+	let yItem = randomValueFromArray(insertY);
+	let zItem = randomValueFromArray(insertZ);
+	
+	newStory = newStory.replace(":insertx:", xItem);
+    newStory = newStory.replace(":inserty:", yItem);
+    newStory = newStory.replace(":insertz:", zItem);
+
+  if(customName.value !== '') {
+    const name = customName.value;
+	newstory = newstory.replace("Bob", name);
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300 * 0.071429)+ "stone";
+    const temperature =  Math.round((94-32) * 5 / 9)+ "centigrade";
+	newStory.replace("weight","300 pounds")
+    newStory.replace("temperature", "94 farenheit")
+
+  }
+
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
