9b4129e52c986402f5a95557884741c643f519ce
jianshuzhangdc <jianshu.zhang@dcmail.ca>
2023-11-24 21:46:14 -0500

    Declaring the array of image filenames

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const alts = {
+  'pic1.jpg' : 'a human eye',
+  'pic2.jpg' : 'looks like a picture',
+  'pic3.jpg' : 'Purple and white flowers',
+  'pic4.jpg' : 'pyramid painting',
+  'pic5.jpg' : 'butterfly'
+}
 
 /* Looping through images */
 
